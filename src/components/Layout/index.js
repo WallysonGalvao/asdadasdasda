30f534368ef29c67f2b312e7a8ac4b6c0066b877
Galvao Da Cunha <wgalvao@indracompany.com>
2020-05-06 22:38:44 -0300

    first commit

---
+++ src/components/Layout/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="font-sans bg-white flex flex-col min-h-screen w-full">
+      <div>
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
